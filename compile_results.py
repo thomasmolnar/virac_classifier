01cfaa4b73b6fba31659bfb27865e8dac4959ca0
thomasmolnar <thomas.molnar@live.com>
2021-02-25 14:36:20 +0000

    dropped duplicates from compiler

--- compile_results.py
+++ compile_results.py
@@ -1,18 +1,43 @@
 import pandas as pd
 import glob
+import re
 
 results_dir = ""
 
-files = [file for file in glob.glob(results_dir+"*.csv.tar.gz") if "_test" not in file]
+files = set([file for file in glob.glob(results_dir+"*.csv.tar.gz") if ("compiled" not in file and "test" not in file)])
 
-df_tot = pd.DataFrame()
+hpx_ind = []
 for i in files:
-    df = pd.read_csv(i, compression="gzip", error_bad_lines=False)
-    df = df.loc[df['class']!='CONST'].copy()
-    df_tot = pd.concat([df_tot, df])
+    temp = re.findall(r'\d+', i)
+    res = list(map(int, temp))
+    _ind = res[0]
+    hpx_ind.append(_ind)
 
-df_tot.to_csv(results_dir+"results_compiled.csv.tar.gz")    
+hpx_ind = set(hpx_ind)    
     
+assert len(files)==len(hpx_ind), "Not matching number of files and Healpix indices."
+    
+df_tot = pd.DataFrame()
+for i,j in zip(files, hpx_ind):
+    try:
+        df = pd.read_csv(i, compression="gzip", error_bad_lines=False)
+    except:
+        try:
+            df = pd.read_csv(i, compression="gzip", error_bad_lines=False, header=None, delim_whitespace=True)
+        except:
+            continue
+        
+    df = df.loc[df['class']!='CONST'].copy()
+    df["hpx_index"] = np.int32(j)
+    df_tot = pd.concat([df_tot, df])
+    
+df_tot_drop = df_tot.drop_duplicates(subset='sourceid')
+print("Compiled: {} initial sources, {} duplicated sources.".format(len(df_tot), len(df_tot)-len(df_tot_drop)))
+
 for i in set(df_tot['class'].values):
-    count = len(df_tot.loc[df_tot['class']==i])
-    print(f"{count} sources of type {i} compiled")
+    _cls = df_tot.loc[df_tot['class']==i].copy()
+    count = len(_cls)
+    num_dups = count - len(_cls.drop_duplicates(subset='sourceid'))
+    print(f"{i}: {count} sources compiled, {num_dups} duplicates.")
+    
+df_tot.to_csv(results_dir+"results_compiled.csv.tar.gz")
